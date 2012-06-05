af04836afc9a116f1acfd8bb8a8a28c991564e78
Nicolas Leleu <n.leleu@hotmail.fr>
2012-06-05 12:45:25 +0200

    Exception

---
+++ Code/projet_lo21/typeexception.h
@@ -0,0 +1,16 @@
+#ifndef TYPEEXCEPTION_H
+#define TYPEEXCEPTION_H
+
+#include <stdexcept>
+#include <string>
+
+class typeException :public std::exception
+{
+    std::string info;
+public:
+    typeException(const char * s):info(s){}
+    const char* what() const throw(){return info.c_str();}
+    ~typeException() throw(){}
+};
+
+#endif // TYPEEXCEPTION_H
